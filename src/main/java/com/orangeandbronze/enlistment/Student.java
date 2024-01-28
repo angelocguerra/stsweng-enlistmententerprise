ba59c037ff03f44ec0897f197dbffe4b49a39ec7
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-01-28 17:08:40 +0800

    Added javadoc comments for better readability

--- src/main/java/com/orangeandbronze/enlistment/Student.java
+++ src/main/java/com/orangeandbronze/enlistment/Student.java
@@ -1,18 +1,30 @@
 package com.orangeandbronze.enlistment;
 
+import static org.apache.commons.lang3.Validate.isTrue;
 import java.util.Objects;
 
-import static org.apache.commons.lang3.Validate.isTrue;
-
+/**
+ * The Domain Model
+ * This class contains the student's ID Number and set of sections enlisted
+ */
 class Student {
     private final int studentNo;
 
+    /**
+     *
+     * @param studentNo - Student's id number
+     */
     Student(int studentNo) {
         isTrue(studentNo >= 0,
                 "studentNumber should be non-negative, was:" + studentNo);
         this.studentNo = studentNo;
     }
 
+    /**
+     *
+     * @param o - object generated from the hashCode
+     * @return true if the student numbers match
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -21,13 +33,21 @@ class Student {
         return studentNo == student.studentNo;
     }
 
+    /**
+     *
+     * @return generated object that serves as the student's id number
+     */
     @Override
     public int hashCode() {
         return Objects.hash(studentNo);
     }
 
+    /**
+     *
+     * @return typecasted studentNo from object to string
+     */
     @Override
     public String toString() {
-        return "Student# " + studentNo;
+        return "Student No.:" + studentNo;
     }
 }
