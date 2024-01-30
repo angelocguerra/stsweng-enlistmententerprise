056dcdd8fe084b0d74653fa2b910eb9174dab729
Josh Ang Ngo Ching <josh_darren_angngoching@dlsu.edu.ph>
2024-01-30 12:04:44 +0800

    added javadocs for the exception classes

--- src/main/java/com/orangeandbronze/enlistment/CancellingUnenlistedSectionException.java
+++ src/main/java/com/orangeandbronze/enlistment/CancellingUnenlistedSectionException.java
@@ -1,7 +1,12 @@
 package com.orangeandbronze.enlistment;
-
+/**
+ * A custom Exception class to handle the situation when a user attempts to cancel an unenlisted section
+ */
 public class CancellingUnenlistedSectionException extends RuntimeException {
-
+    /**
+     * Creates a CancellingUnenlistedSectionException with a specified message
+     * @param message   A detailed message explaining the reason this error is thrown
+     */
     public CancellingUnenlistedSectionException(String message) {
         super(message);
     }
--- src/main/java/com/orangeandbronze/enlistment/RoomCapacityReachedException.java
+++ src/main/java/com/orangeandbronze/enlistment/RoomCapacityReachedException.java
@@ -1,7 +1,12 @@
 package com.orangeandbronze.enlistment;
-
+/**
+ * A custom Exception class to handle the situation when a user attempts to enlist in a section whose room is already full
+ */
 public class RoomCapacityReachedException extends RuntimeException {
-
+    /**
+     * Creates a RoomCapacityReachedException with a specified message
+     * @param message   A detailed message explaining the reason this error is thrown
+     */
     public RoomCapacityReachedException(String message) {
         super(message);
     }
