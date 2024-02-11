11255fa75b117ebd43c23cbf628b0a155f5ee9f0
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-02-11 23:53:28 +0800

    feat: Added DegreeProgram class

---
+++ src/main/java/com/orangeandbronze/enlistment/DegreeProgram.java
@@ -0,0 +1,21 @@
+package com.orangeandbronze.enlistment;
+
+import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.Validate.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+
+class DegreeProgram {
+    private final String degreeProgramName;
+    private final Collection<Subject> degreeProgramSubjects = new HashSet<>();
+
+    DegreeProgram(String degreeProgramName, Collection<Subject> degreeProgramSubjects) {
+        notBlank(degreeProgramName);
+        requireNonNull(degreeProgramSubjects);
+        this.degreeProgramSubjects.addAll(degreeProgramSubjects);
+        this.degreeProgramSubjects.removeIf(Objects::isNull);
+        this.degreeProgramName= degreeProgramName;
+    }
+}
