badba291aacf541635aeafdd0cd71320c87e711c
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-02-16 17:42:34 +0800

    Revert "Added ScheduleRoomConflictException"
    This reverts commit 7dedf8997af7a29e5de97d52bb10baeead933e37.

--- src/main/java/com/orangeandbronze/enlistment/ScheduleRoomConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.orangeandbronze.enlistment;
-
-public class ScheduleRoomConflictException extends RuntimeException {
-    ScheduleRoomConflictException(String msg) {
-        super(msg);
-    }
-}
